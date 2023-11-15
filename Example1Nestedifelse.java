a950064fbd5dcd594ff5c3365a93fb8c8f468a74
Muktanali Ghangale <150799174+muktabhagwat@users.noreply.github.com>
2023-11-15 18:24:07 +0530

    Update and rename Job.java to Example1Nestedifelse.java

--- Example1Nestedifelse.java
+++ Example1Nestedifelse.java
@@ -2,7 +2,7 @@ package basicjava;
 
 import java.util.Scanner;
 
-public class Job {
+public class Example1Nestedifelse {
 
 	public static void main(String[] args) 
 	{
@@ -11,26 +11,27 @@ public class Job {
 		
 		Scanner sc=new Scanner(System.in);
 		
-		System.out.println("1: Freshers\n2:Expert");
+		System.out.println("\nEnter your choice\n1: Freshers\n2:Expert");
 		interview_cat=sc.nextInt();
 		if(interview_cat==1)
 		{
 			System.out.println("The company will decide the job profile according to the interview");	
 		}
-		if(interview_cat==2)
+		else
 		{
 			System.out.println("Choice your profile \nJ:java developer\nA:.netDeveloper");
 			choice=sc.next().charAt(0);
-		}
-		if(choice=='J')
-		{
-			System.out.println("You have selected the designation ad Java Developer");
-		}
-		if(choice=='A')
-		{
-			System.out.println("You have selected the designation as .net developer");
+			if(choice=='J')
+			{
+			
+				System.out.println("You have selected the designation ad Java Developer");
+			}
+			else
+			{
+				System.out.println("You have selected the designation as .net developer");
+			}
 		}
 		sc.close();
 	}
-
 }
+
